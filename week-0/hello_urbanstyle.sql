39ffe2b6283b04a62635a885df63c4cc8e5330ca
Fatima Rahimi <fatimarahimi5696@gmail.com>
2026-08-18 15:27:25 +0300

    Add Week 0 hello_urbanstyle.sql

---
+++ week-0/hello_urbanstyle.sql
@@ -0,0 +1,5 @@
+-- Week 0: first Supabase query
+SELECT 'Hello, UrbanStyle.ltd!' AS greeting, NOW() AS checked_at;
+
+-- Created the team_members practice table and added the team:
+SELECT * FROM team_members ORDER BY id;
